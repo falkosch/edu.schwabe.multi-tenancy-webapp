2a2d4de2b500d16b9735de378f97d775436ef29e
Falko Schwabe <falkosch@hotmail.de>
2018-11-10 17:53:56 +0100

    remove state change delayíng resolve for testing purposes

--- prototype/src/login/login.route.js
+++ prototype/src/login/login.route.js
@@ -1,4 +1,3 @@
-import angular from 'angular';
 import { LoginName } from './login.component';
 
 loginRoute.$inject = ['$stateProvider'];
@@ -9,12 +8,7 @@ export function loginRoute($stateProvider) {
     $stateProvider
         .state({
             name: LoginStateId,
-            resolve: {
-                longAsync: [
-                    '$timeout',
-                    $timeout => $timeout(angular.noop, 1000),
-                ],
-            },
+            resolve: {},
             url: '/login',
             views: {
                 main: LoginName,
