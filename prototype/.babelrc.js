f1e0ada1ef6942cbb43b2c54b03d4e093b910c85
Falko Schwabe <falkosch@hotmail.de>
2018-11-10 04:00:16 +0100

    use a babel plugin for partially importing lodash

--- prototype/.babelrc.js
+++ prototype/.babelrc.js
@@ -12,6 +12,7 @@ module.exports = (api) => {
         plugins: [
             '@babel/plugin-transform-runtime',
             '@babel/plugin-proposal-class-properties',
+            'lodash',
         ],
         env: {
             test: {
--- prototype/src/header/nav/nav.service.spec.js
+++ prototype/src/header/nav/nav.service.spec.js
@@ -1,4 +1,4 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
 
 import { NavServiceName, NavService } from './nav.service';
 import { NavModule } from './nav.module';
