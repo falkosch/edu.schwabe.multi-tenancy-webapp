b757f135eb71c2e2c4207d40b2a3fbc41bd49614
Falko Schwabe <falkosch@hotmail.de>
2018-11-18 01:39:30 +0100

    extend menubar tests

--- prototype/src/menubar/menubar.controller.spec.js
+++ prototype/src/menubar/menubar.controller.spec.js
@@ -4,22 +4,102 @@ import { MenubarController } from './menubar.controller';
 
 describe(`${MenubarModule}.${MenubarName} component controller`, () => {
 
+    const $stateMockWithTitle = {
+        current: {
+            data: {
+                title: 'mocked',
+            },
+            name: 'mockedName',
+        },
+    };
+
+    const $stateMockWithOnlyName = {
+        current: {
+            name: 'mockedOnlyName',
+        },
+    };
+
     let menubarController;
 
     beforeEach(() => {
 
         angular.mock.module(MenubarModule);
 
-        inject(($componentController) => {
-            menubarController = $componentController(MenubarName);
-        });
-
     });
 
     it(`should be an instanceof ${MenubarName} component controller`, () => {
 
-        expect(menubarController)
-            .toEqual(jasmine.any(MenubarController));
+        inject(($componentController) => {
+
+            menubarController = $componentController(MenubarName, {
+                $state: $stateMockWithTitle,
+            });
+
+            expect(menubarController)
+                .toEqual(jasmine.any(MenubarController));
+
+        });
+
+    });
+
+    describe('.currentStateTitle', () => {
+
+        it('should return a string of a current state title', () => {
+
+            inject(($componentController) => {
+
+                menubarController = $componentController(MenubarName, {
+                    $state: $stateMockWithTitle,
+                });
+
+                expect(menubarController.currentStateTitle)
+                    .toEqual(jasmine.any(String));
+
+            });
+
+        });
+
+        it('should not return the state name when there is a "data.title" set', () => {
+
+            inject(($componentController) => {
+
+                menubarController = $componentController(MenubarName, {
+                    $state: $stateMockWithTitle,
+                });
+
+                expect(menubarController.currentStateTitle)
+                    .toEqual($stateMockWithTitle.current.data.title);
+
+                expect(menubarController.currentStateTitle)
+                    .not.toEqual($stateMockWithTitle.current.name);
+
+                expect(menubarController.currentStateTitle)
+                    .not.toEqual($stateMockWithOnlyName.current.name);
+
+            });
+
+        });
+
+        it('should return the state name when there is no "data.title" set', () => {
+
+            inject(($componentController) => {
+
+                menubarController = $componentController(MenubarName, {
+                    $state: $stateMockWithOnlyName,
+                });
+
+                expect(menubarController.currentStateTitle)
+                    .toEqual($stateMockWithOnlyName.current.name);
+
+                expect(menubarController.currentStateTitle)
+                    .not.toEqual($stateMockWithTitle.current.data.title);
+
+                expect(menubarController.currentStateTitle)
+                    .not.toEqual($stateMockWithTitle.current.name);
+
+            });
+
+        });
 
     });
 
