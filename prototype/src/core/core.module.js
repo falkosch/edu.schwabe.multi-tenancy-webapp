5cdb128561fbf64fb516e701928da6c00e1d1578
Falko Schwabe <falkosch@hotmail.de>
2019-02-10 03:01:00 +0100

    minor code clean up

--- prototype/src/core/core.module.js
+++ prototype/src/core/core.module.js
@@ -3,12 +3,12 @@ import angular from 'angular';
 import { BackendModule } from './backend/backend.module';
 import { MockBackendModule } from './mock-backend/mock-backend.module';
 
-import { PromiseTrackerModule } from './promise-tracker/promise-tracker.module';
-import { UserStateModule } from './user-state/user-state.module';
-import { EventEmitterModule } from './event-emitter/event-emitter.module';
 import { AccessControlModule } from './access-control/access-control.module';
+import { EventEmitterModule } from './event-emitter/event-emitter.module';
 import { LanguageModule } from './language/language.module';
 import { NavigationModule } from './navigation/navigation.module';
+import { PromiseTrackerModule } from './promise-tracker/promise-tracker.module';
+import { UserStateModule } from './user-state/user-state.module';
 
 export const CoreModule = angular
     .module('app.core', [
@@ -16,10 +16,10 @@ export const CoreModule = angular
         MockBackendModule,
 
         AccessControlModule,
-        PromiseTrackerModule,
         EventEmitterModule,
-        UserStateModule,
         LanguageModule,
         NavigationModule,
+        PromiseTrackerModule,
+        UserStateModule,
     ])
     .name;
