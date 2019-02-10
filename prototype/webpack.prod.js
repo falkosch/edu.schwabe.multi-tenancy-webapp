4d5d47decb221c412592e9ce0048e63d2738f8b2
Falko Schwabe <falkosch@hotmail.de>
2019-02-11 00:00:30 +0100

    mangle import names for bit smaller file sizes (needs verification)

--- prototype/webpack.prod.js
+++ prototype/webpack.prod.js
@@ -39,6 +39,7 @@ module.exports = env => common(env)
             }),
         ],
         optimization: {
+            mangleWasmImports: true,
             minimizer: [
                 new TerserPlugin({
                     cache: true,
