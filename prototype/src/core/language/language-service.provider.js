9374a6e52f68163bf6008be4ec6c1b662d42109f
Falko Schwabe <falkosch@hotmail.de>
2019-03-10 00:02:19 +0100

    remove unused variable

--- prototype/src/core/language/language-service.provider.js
+++ prototype/src/core/language/language-service.provider.js
@@ -15,7 +15,7 @@ export class LanguageServiceProvider {
         (...injectables) => new LanguageService(...injectables),
     );
 
-    constructor($translatePartialLoaderProvider, $injector) {
+    constructor($translatePartialLoaderProvider) {
         this.$translatePartialLoaderProvider = $translatePartialLoaderProvider;
     }
 
